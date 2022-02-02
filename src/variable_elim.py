2cf23afaa3087530664e3bf55aa5109a8ffde10b
adkamin <minichovaa@gmail.com>
2022-02-02 19:59:06 +0100

    Simple factor operations

--- src/variable_elim.py
+++ src/variable_elim.py
@@ -4,7 +4,7 @@
 Class for the implementation of the variable elimination algorithm.
 """
 
-from pprint import pprint
+import pprint
 
 
 class VariableElimination():
@@ -14,51 +14,51 @@ class VariableElimination():
         Initialize the variable elimination algorithm with the specified network.
         Add more initializations if necessary.
         """
-        self.network = network # object of class BayesNet
-        self.factors = self.network.probabilities # dictionary (keys are variables)
 
-        # Testing things out
-        alarm = self.network.probabilities['Alarm'] # indexing key 'Alarm'
+        self.factors = network.probabilities # dictionary (keys are variables)
 
-        print('-----------------')
-        print(alarm)
-        alarm.drop(alarm.index[alarm['Burglary'] == 'True'], inplace = True) # removing all rows where B=T
-        print('-----------------')
-        print(alarm)
-        print('----------------')
-
-    # def init_hidden_vars(self, query, observed):
-    #     """ 
-    #     Initialize the hidden variables based on given query and observed variables
-    #     """
-    #     hidden_vars = dict(self.network.probabilities)
-    #     for o in observed:
-    #         if o in hidden_vars.keys():
-    #             hidden_vars.pop(o)
-    #     hidden_vars.pop(query)
-    #     self.hidden_vars = hidden_vars
-
-    #     print(f'The hidden variables are: ', end=' ')
-    #     for k in hidden_vars.keys():
-    #         print(k, end=' ')
 
     def update_factors(self, observed):
         """ 
         Initialize factors, based on the observation
         """
 
-        # TODO: rename factors as in dictionary "probabilities". Let each key be a tuple
-        # of factor index and list of variables within the factor.
-        # Index is necessary, if we ever encounter more factors with the same variables inside
-
+        # Represent factors with keys with an index and a tuple of involved variables
+        # Index is useful once there are factors that contain the same variables
+        # Involved variables are useful for eliminating variables in the VE algorithm
         factors_copy = dict.copy(self.factors)
+        new_factors = {}
+        i = 0
+        for key in factors_copy:
+            new_factors[(i, tuple(factors_copy[key].columns[:-1]))] = self.factors.pop(key)
+            i += 1
+        # Reduce the factors given the observation
+        self.factors = new_factors
         for o in observed:
-            for f in factors_copy:
-                print(type(f.keys()))
-                # for k in list(f.keys()):
-                #     if o == k:
-                #         print('----------------------------')
-                #         print(f'{o} is present in table {f}')
+            for key in self.factors:
+                if o in key[1]:
+                    self.factors[key].drop(self.factors[key].index[self.factors[key][o] != observed[o]],
+                     inplace = True)
+
+
+    def get_factors(self, var):
+        """ 
+        Return factors which contain variable var
+        """
+        
+        factors = []
+        for key in self.factors:
+            if var in key[1]:
+                factors.append(self.factors[key])
+        return factors
+
+
+    def multiply(self, factors):
+        """
+        Return a factor which is product of factors
+        """
+
+
 
     def run(self, query, observed, elim_order):
         """
@@ -76,13 +76,34 @@ class VariableElimination():
                 for the query variable
         """
 
-        print(f'The query variable is: {query}')
-        if observed: 
-            print(f'The observed variables are: {observed}')
-        
-        self.update_factors(observed)
+        print('-------------------------------------------------------')
+        print('Variable Elimination Algorithm with logs of steps taken')
+        print('-------------------------------------------------------')
 
-        # TODO: Print formulas
-        # print(f'Product formula to compute the query: P({query} | {observed}) = \u03A3_A,B,C ')
+        print(f'\nA) The query variable: {query}\n')
+        if observed: 
+            print(f'B) The observed variables: {observed}\n')
+        else:
+            print(f'B) There are no observed variables\n')
+        print('C) The formula to be computed: ...\n')
+        self.update_factors(observed)
+        print('D) The factors:\n')
+        pprint.pprint(self.factors)
+        print(f'\nE) The elimination ordering: {elim_order}\n')
+
+        print(f'-----------------------')
+        print(f'F) The elimination part')
+        print(f'-----------------------')
+
+        i = len(self.factors) # Currently the highest factor index
+        for v in elim_order:
+            print(f'\nVariable to be eliminated: {v}')
+            if v != query:
+                factors_with_v = self.get_factors(v)
+                # new_factor = self.multiply(factors_with_v)
+                # reduced_factor = self.sum_out(v, factors_with_v)
+                # remove factors_with_v from self.factors, add reduced factor to self.factors (with new index)
+                i += 1
+
 
 
